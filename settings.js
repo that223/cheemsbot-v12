ffdda218d106665737c86e58c5828ec6278805b7
that223 <166918609+that223@users.noreply.github.com>
2024-05-11 01:17:57 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,7 +16,7 @@ global.socialm = "GitHub: Debashis121212" //ur github or insta name
 global.location = "Nalhati, Birbhum, WB,INDIA  " //ur location
 
 //new
-global.botname = '꧁﴿·⁎̩͙ ⁑̩͙̩͙ ⁂̩̩͙»✥«·-𝕯𝕯 𝕮𝖍𝖊𝖊𝖒𝖘-𝕭𝖔𝖙-·»✥«⁂̩̩͙͙ ⁑̩͙̩͙ ⁎̩͙.·﴾꧂' //ur bot name
+global.botname = '꧁﴿·⁎̩'CheemsBot bot V12' //ur bot name
 global.ownernumber = '22395064497' //ur owner number
 global.ownername = '𝑏𝑤𝑎𝑏𝑦𝑚𝑜𝑢𝑑𝑦♨︎' //ur owner name
 global.websitex = "https://www.facebook.com/Debashi.Dey.X1/"
